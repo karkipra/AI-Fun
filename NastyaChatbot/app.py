46c56b614a0175947143d1acc1c6f6bd189a5733
Pratik Karki <karkipra@grinnell.edu>
2020-05-12 22:00:30 -0400

    adding new files with better memory usage:

--- NastyaChatbot/app.py
+++ NastyaChatbot/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request  # import flask
 from random import randint
-import pandas as pd
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 import re
+import pickle
 
 # Constant for randomness
 RANDOMNESS = 5
 
 # Parse the auth and host from env:
-bonsai = "https://jcky5zh1gq:3qkm3gjy40@cherry-213410966.us-east-1.bonsaisearch.net:443"
+bonsai = "https://ne5qu8h33j:ykwfoyb12z@dogwood-455186479.us-east-1.bonsaisearch.net:443"
 auth = re.search('https://(.*)@', bonsai).group(1).split(':')
 host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
 
@@ -37,14 +37,20 @@ es = Elasticsearch(es_header)
 if not es.ping():
     print("Connection Error!")
 
-# train_data instance and setting up with ES
-train_data = pd.read_csv('data/train_data_2.csv')
-train_data.dropna(subset=['text', 'response'], inplace=True)
-texts_dict = train_data.to_dict(orient='records')
+"""
+train_data instance and setting up with ES
+Reuse this code if you make changes to the train_data to re-pickle it later
+Might need to run the Jupyter Notebook code then
+"""
+# train_data = pd.read_csv('data/train_data_2.csv')
+# train_data.dropna(subset=['text', 'response'], inplace=True)
+# texts_dict = train_data.to_dict(orient='records')
 
-# Why is this not working??
+# using Pickled Data for optimization
+with open('data/train_data.pickle', 'rb') as handle:
+    texts_dict = pickle.load(handle)
+
 bulk(es, texts_dict, index='chatbot-events', doc_type='clue', raise_on_error=True)
-
 
 # create an app instance
 app = Flask(__name__)
@@ -60,14 +66,14 @@ def get_bot_response():
     user_text = request.args.get('msg')
     return chat(user_text)
 
+
 """
 chat - receives user_text and does a search for matching terms
 params - user_text (String)
 """
 
-def chat(user_text):
-    # return user_text
 
+def chat(user_text):
     # Get response from ES
     response = es.search(index='chatbot-events', doc_type='clue', body={"query": {
         "match": {
