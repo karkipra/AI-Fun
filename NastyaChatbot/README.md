30644bd997c935637a4d6e9c89514981fde24cbf
Pratik Karki <karkipra@grinnell.edu>
2020-05-13 21:22:20 -0400

    adding updates to readme

--- NastyaChatbot/README.md
+++ NastyaChatbot/README.md
@@ -1,6 +1,6 @@
 # Nastya Chatbot
 
-__Update__: Version 3.0 is now available! Check it out here: https://nastya-chatbot.herokuapp.com/
+__Update__: Version 3.0 makes the chatbot compatible with Heroku! However, the Heroku add-on doesn't allow much data usage in their free mode hehe so I'm sticking to local development for now. Stay tuned! 
 
 __Update__: Version 2.0 is out! Newer, bigger, and cleaner data.
 
@@ -14,44 +14,49 @@ The main principle of the chatbot was TF-IDF, aided greatly by [Elasticsearch](h
 
 ![TF-IDF Image](https://pathmind.com/images/wiki/tfidf.png)
 
+Source: Wikipedias
+
+All the data was processed in Jupyter Notebook. Please refer to `data_prep.ipynb` and `chatbot.ipynb` for details on data processing.
+
 ## Demo
 
 ![Demo](demo.png)
 
-## Code sample
+## Installation
 
-Below is my implementation of the chatbot function (of course preceded by a lot of data processing)
+The application can be installed in a few easy steps. First, clone the repository:
 
-```
-# Randomness indictator for the chatbot
-randomness = 10
+`git clone https://github.com/karkipra/AI-Fun.git`
 
-def chatbot():
-    run=True
-    while run:
-        text = str(input('Me: '))
-        ##an optional quit command
-        if text == 'quit':
-            run=False
-        else:
-            response = es.search(index='textbot', doc_type='clue', body={ "query": {
-                "match": {
-                    "text":text
-                }
-            }})
-            try:
-                ##introduce a bit of randomness into the response 
-                i = randint(0,randomness)
-                print("Nastya: %s" % response['hits']['hits'][i]['_source']['response'])
-            except:
-                print("Nastya: LOST FOR WORDS")
-```
+The app is located within a subdirectory of this project so lets `cd` into it:
+
+`cd NastyaChatbot`
+
+Now we want the libraries and modules that are used by the app to be installed. If you have `pip`, it's a piece of cake.
+ 
+ ```
+pip install elasticsearch
+pip install pickle     
+``` 
+
+The `templates` directory contains `home.html` which you can modify to change the look of the app. `app.py` is where the main meat of the application lies. Please refer to that file if you wish to learn more about how the app works. 
+
+Finally, to run the app:
+
+`python app.py`
+
+Open the app on http://127.0.0.1:5000/ and Voila!
 
 ## References
 
 - https://towardsdatascience.com/how-to-build-an-easy-quick-and-essentially-useless-chatbot-using-your-own-text-messages-f2cb8b84c11d
+- https://devcenter.heroku.com/articles/bonsai
+- https://docs.bonsai.io/article/94-connecting-to-bonsai
 
-## Updates
+## Future Updates
 
 - Incorporate audio messages and facebook comments
-- Wrinkled out some issues with the randomness of the chatbot
+- Wrinkle out some issues with the randomness of the responses
+- Fix issue with Bonsai Add-on data usage and deploy it successfully
+    - Possibly use a database like SQLite to save memory 
+
--- NastyaChatbot/app.py
+++ NastyaChatbot/app.py
@@ -5,77 +5,45 @@ from elasticsearch.helpers import bulk
 import re
 import pickle
 
-# Constant for randomness
+# Constant for randomness of bot results
 RANDOMNESS = 5
 
-# Parse the auth and host from env:
-bonsai = "https://ne5qu8h33j:ykwfoyb12z@dogwood-455186479.us-east-1.bonsaisearch.net:443"
-auth = re.search('https://(.*)@', bonsai).group(1).split(':')
-host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
+# Global Elasticsearch object
+ES = None
 
-# optional port
-match = re.search('(:\d+)', host)
-if match:
-    p = match.group(0)
-    host = host.replace(p, '')
-    port = int(p.split(':')[1])
-else:
-    port = 443
+# Bonsai Url
+BONSAI_URL = "https://5a4z2itkpp:bf5bs55wlx@azalea-318561537.us-east-1.bonsaisearch.net:443"
 
-# Connect to cluster over SSL using auth for best security:
-es_header = [{
-    'host': host,
-    'port': port,
-    'use_ssl': True,
-    'http_auth': (auth[0], auth[1])
-}]
-
-# Instantiate the new Elasticsearch connection:
-es = Elasticsearch(es_header)
-
-# Verify that Python can talk to Bonsai (optional):
-if not es.ping():
-    print("Connection Error!")
-
-"""
-train_data instance and setting up with ES
-Reuse this code if you make changes to the train_data to re-pickle it later
-Might need to run the Jupyter Notebook code then
-"""
-# train_data = pd.read_csv('data/train_data_2.csv')
-# train_data.dropna(subset=['text', 'response'], inplace=True)
-# texts_dict = train_data.to_dict(orient='records')
-
-# using Pickled Data for optimization
-with open('data/train_data.pickle', 'rb') as handle:
-    texts_dict = pickle.load(handle)
-
-bulk(es, texts_dict, index='chatbot-events', doc_type='clue', raise_on_error=True)
-
-# create an app instance
+# Create an app instance
 app = Flask(__name__)
 
 
-@app.route("/")  # at the end point /
+# at the end point /
+@app.route("/")
 def home():
     return render_template('home.html')
 
 
+# GET request from user
 @app.route("/get")
 def get_bot_response():
+    """
+    get_bot_response - parses the request from userInput and returns the chat method
+    params - None
+    returns - response (String)
+    """
     user_text = request.args.get('msg')
     return chat(user_text)
 
 
-"""
-chat - receives user_text and does a search for matching terms
-params - user_text (String)
-"""
-
-
 def chat(user_text):
+    """
+    chat - receives user_text and does a search for matching terms
+    params - user_text (String)
+    returns - response (String)
+    """
     # Get response from ES
-    response = es.search(index='chatbot-events', doc_type='clue', body={"query": {
+    response = ES.search(index='chatbot-events', doc_type='clue', body={"query": {
         "match": {
             "text": user_text
         }
@@ -89,5 +57,54 @@ def chat(user_text):
         return "LOST FOR WORDS"
 
 
-if __name__ == "__main__":  # on running python app.py
-    app.run(debug=True)  # run the flask app
+def create_elasticsearch(bonsai):
+    """
+    create_elasticsearch - creates elasticsearch instance from the bonsai cluster
+    params - bonsai (String)
+    returns - es (Elasticsearch Object)
+    """
+    # Parse the auth and host from env:
+    auth = re.search('https://(.*)@', bonsai).group(1).split(':')
+    host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
+
+    # optional ports
+    match = re.search('(:\d+)', host)
+    if match:
+        p = match.group(0)
+        host = host.replace(p, '')
+        port = int(p.split(':')[1])
+    else:
+        port = 443
+
+    # Connect to cluster over SSL using auth for best security:
+    es_header = [{
+        'host': host,
+        'port': port,
+        'use_ssl': True,
+        'http_auth': (auth[0], auth[1])
+    }]
+
+    # Instantiate the new Elasticsearch connection:
+    return Elasticsearch(es_header)
+
+
+def bulk_data(es):
+    """
+    bulk_data - retrieves training data from pickle and uses BULK to index the data
+    params - es (Elasticsearch Instance)
+    returns - None
+    """
+    # using Pickled Data for optimization
+    with open('data/train_data.pickle', 'rb') as handle:
+        texts_dict = pickle.load(handle)
+
+    bulk(es, texts_dict, index='chatbot-events', doc_type='clue', raise_on_error=True)
+
+
+if __name__ == "__main__":
+    # Create elasticsearch instance
+    ES = create_elasticsearch(BONSAI_URL)
+    # Bulk the indices of the data
+    bulk_data(ES)
+    # runs the flask app
+    app.run(debug=True)
